c6bd88ea3c695ab12fafde02c1613fe5c9684847
Mindbeat <142962460+MindBeatCompany@users.noreply.github.com>
2024-07-08 00:07:14 -0300

    Update env.js

--- src/config/env.js
+++ src/config/env.js
@@ -1,6 +1,6 @@
 // env.js
 
 export const API_PORT = process.env.API_PORT || 8083;
-export const API_URL = process.env.API_URL 
+export const API_URL = process.env.API_URL || "https://back-test.derservicios.com.ar"
 
 
